d002223e8e9550b4f9abe436e13f612939fcadc7
GiacWebDev <giacobechini@gmail.com>
2023-11-08 00:32:08 +0100

    [add] file Production

---
+++ models/Production.php
@@ -0,0 +1,16 @@
+<?php
+class Production {
+  public $name;
+  public $genre;
+  public $vote;
+  
+  public function __construct(string $_name, string $_genre, $_vote) {
+    $this->name = $_name;
+    $this->genre = $_genre;
+    $this->vote = $_vote;
+  }
+
+  public function getVote() {
+    return $this->vote;
+  }
+}
